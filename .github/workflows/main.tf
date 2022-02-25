e80a02ddcf5c2c44c21de6db9fd8e0fb23219219
uk-bolly <69214557+uk-bolly@users.noreply.github.com>
2022-02-25 14:54:06 +0000

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -9,6 +9,15 @@ data "aws_vpc" "default" {
   default = true
 }
 
+resource "random_id" "server" {
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    ami_id = "${var.ami_id}"
+  }
+
+  byte_length = 8
+}
+
 resource "aws_security_group" "github_actions" {
   name   = "${var.namespace}-${random_id.server.hex}"
   vpc_id = data.aws_vpc.default.id
